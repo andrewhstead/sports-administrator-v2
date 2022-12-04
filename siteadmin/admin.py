f3f184dbbca7bb66b4af466b1a50dc8212b93b0c
Andrew Stead <andrew@andrewstead.co.uk>
2022-12-04 21:08:54 +0000

    Added custom user model and recreated database.

--- siteadmin/admin.py
+++ siteadmin/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Sport, Country, State, City, Venue
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Sport)
+admin.site.register(Country)
+admin.site.register(State)
+admin.site.register(City)
+admin.site.register(Venue)
--- siteadmin/models.py
+++ siteadmin/models.py
@@ -1,7 +1,32 @@
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.db import models
+
+# Options for different types of site, to determine which modules are available.
+SITE_OPTIONS = (
+    ('individual', "Individual Player"),
+    ('club', "Single Club"),
+    ('competition', "Single Competition"),
+    ('multi', "Multiple Competitions"),
+)
 
 
 # Create your models here.
+
+# Additional fields are added to the AbstractUser model.
+class User(AbstractUser):
+    objects = UserManager()
+    organisation = models.CharField(max_length=50, blank=True, null=True)
+    site_name = models.CharField(max_length=50, blank=True, null=True)
+    site_type = models.CharField(max_length=25, choices=SITE_OPTIONS, blank=True, null=True)
+    primary_color = models.CharField(max_length=10, blank=True, null=True, default="#ffffff")
+    secondary_color = models.CharField(max_length=10, blank=True, null=True, default="#ffffff")
+    primary_text = models.CharField(max_length=10, blank=True, null=True, default="#000000")
+    secondary_text = models.CharField(max_length=10, blank=True, null=True, default="#000000")
+
+    def __unicode__(self):
+        return self.username
+
+
 class Sport(models.Model):
     objects = models.Manager()
     name = models.CharField(max_length=25)
